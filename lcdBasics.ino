b90e664ff7da17a68239fd1f244c6a3efe2c948e
remitche <remitchegrd@gmail.com>
2023-01-18 15:26:44 +0100

    Update lcdBasics.ino
    prise en compte de la ligne 1

--- lcdBasics.ino
+++ lcdBasics.ino
@@ -22,12 +22,23 @@ void loop()
 
 void showMessage(String line1, String line2)
 {
+  
   	/* Les lignes sont affiché et centrée automatiquement par rapport à leurt taille
     , le curseur se place automatiquement
     */
-	lcd.setCursor((16 - line1.length()) / 2, 0);
+	/* ligne1 */
+  if(line1.length() < 16)
+  {
+    lcd.setCursor((16 - line1.length()) / 2, 0);
     lcd.print(line1);
-    
+  }
+  else
+  {
+    lcd.setCursor(0, 0);
+    lcd.print(line2);
+  }
+  
+  /*ligne2*/
   if(line2.length() < 16)
   {
     lcd.setCursor((16 - line2.length()) / 2, 1);
